92e5bb3128eb56dfaad0c11b222b323e266629d0
Joe Irizarry <joeizy@users.noreply.github.com>
2023-01-01 12:30:40 -0500

    remove reference to App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import TryApp from './try/Try';
 import TryPlotlyApp from './try/TryPlotly';
@@ -13,7 +12,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     {/* <TryApp /> */}
     {/* <TryPlotlyApp /> */}
     {/* <TryPlotlyFunctional /> */}
