0b491d3c62e6cfa3bf65f58c4f802a8fa3b02247
Joe Irizarry <joeizy@users.noreply.github.com>
2023-01-01 11:17:00 -0500

    Plotly working for charts

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import TryApp from './try/Try';
+import TryPlotlyApp from './try/TryPlotly';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +12,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <TryApp />
+    {/* <TryApp /> */}
+    <TryPlotlyApp />
   </React.StrictMode>
 );
 
---
+++ src/try/TryPlotly.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PivotTableUI from 'react-pivottable/PivotTableUI';
+import 'react-pivottable/pivottable.css';
+import TableRenderers from 'react-pivottable/TableRenderers';
+import Plot from 'react-plotly.js';
+import createPlotlyRenderers from 'react-pivottable/PlotlyRenderers';
+
+// create Plotly renderers via dependency injection
+const PlotlyRenderers = createPlotlyRenderers(Plot);
+
+// see documentation for supported input formats
+const data = [['left', 'right'], ['value1a', 'value1b'], ['value2a', 'value2b']];
+
+class TryPlotlyApp extends React.Component {
+    constructor(props: any) {
+        super(props);
+        this.state = props;
+    }
+
+    render() {
+        return (
+            <PivotTableUI
+                data={data}
+                onChange={s => this.setState(s)}
+                renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
+                {...this.state}
+            />
+        );
+    }
+}
+
+export default TryPlotlyApp
